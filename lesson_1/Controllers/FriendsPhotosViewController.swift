71e867cedc0f7edc68454419668e2440b6859bfd
Stanislav Slip <kaopuk@gmail.com>
2024-03-23 17:24:19 +0300

    add FriendsPhotosViewController and friendsTableView

---
+++ lesson_1/Controllers/FriendsPhotosViewController.swift
@@ -0,0 +1,44 @@
+//
+//  FriendsPhotosViewController.swift
+//  lesson_1
+//
+//  Created by Stanislav on 23.03.2024.
+//
+
+import UIKit
+
+class FriendsPhotosViewController: UIViewController {
+    
+    private var friendNameLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 8, y: 150, width: 150, height: 15))
+        
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .brown
+        setupViews()
+    }
+    
+    private func setupViews() {
+        view.addSubview(friendNameLabel)
+    }
+    
+    func setupFriend(name: String) {
+        friendNameLabel.text = name
+        view.addSubview(friendNameLabel)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- lesson_1/Controllers/FriendsViewController.swift
+++ lesson_1/Controllers/FriendsViewController.swift
@@ -8,15 +8,54 @@
 import UIKit
 
 class FriendsViewController: UIViewController {
+    
+    let array = ["first", "second", "third", "fourth"]
+    let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupViews()
+        setupTableView()
     }
     
-    func setupViews() {
-        self.view.backgroundColor = #colorLiteral(red: 0.5810584426, green: 0.1285524964, blue: 0.5745313764, alpha: 1)
+    private func setupTableView() {
+        tableView.frame = view.frame
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
+        
+        tableView.register(FriendsListTableViewCell.self, forCellReuseIdentifier: "FriendsListCellIdentifier")
+        
+    }
+    
+    @objc private func buttonPressed(sender: UIButton) {
+        print("button pressed")
+        let testVC = FriendsPhotosViewController()
+        self.navigationController?.pushViewController(testVC, animated: true)
     }
 
+}
+
+extension FriendsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let friendPhotosVC = FriendsPhotosViewController()
+        friendPhotosVC.setupFriend(name: array[indexPath.row])
+        
+        navigationController?.pushViewController(friendPhotosVC, animated: true)
+    }
+}
+
+extension FriendsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FriendsListCellIdentifier", for: indexPath) as! FriendsListTableViewCell
+        cell.friendNameLabel.text = array[indexPath.row]
+        
+        return cell
+    }
+    
+    
 }
